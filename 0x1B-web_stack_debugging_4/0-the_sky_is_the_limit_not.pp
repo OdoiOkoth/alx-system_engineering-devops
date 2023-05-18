73380029806b2f54dce6cd6f0638e6443c65b243
OdoiOkoth <odoiokoth@gmail.com>
2023-05-18 22:01:56 +0000

    making nginx handle multiple requests at the same time

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#These lines of code fix an nginx site that can not manage to handle multiple requests concurrently.
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
